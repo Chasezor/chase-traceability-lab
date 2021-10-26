b406be5cb00acdb78f8a203670b98920bded16b5
Chasezor <chase.c.contact@gmail.com>
2021-10-26 14:47:38 -0600

    make it better

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+app = express()
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`)
+})
